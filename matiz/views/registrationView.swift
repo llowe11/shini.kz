c2bdb3f639e52f1e935ae574390f60acfd046747
llowe11 <vladislavkim566@gmail.com>
2024-12-20 22:48:06 +0500

    feat: add registration view fields

--- matiz/views/registrationView.swift
+++ matiz/views/registrationView.swift
@@ -2,8 +2,27 @@ import SwiftUI
 
 struct registrationView: View {
     @AppStorage("isRegistered") private var isRegistered: Bool = false
+    @State var login:String = ""
+    @State var password:String = ""
     var body: some View {
-        Text("Registration view")
+        VStack{
+            TextField("login", text: $login)
+                .border(Color.black, width: 1)
+            TextField("password", text: $password)
+                .border(Color.black, width: 1)
+            HStack{
+                Button {
+                    func register(){
+                        return
+                    }
+                } label: {
+                    Text("submit")
+                        .foregroundStyle(Color.black)
+                        .border(Color.black, width: 1)
+                }
+
+            }
+        }.padding()
     }
 }
 
