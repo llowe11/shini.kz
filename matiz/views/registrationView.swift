5816d73b888cea58772deeb97b4869b837031c88
llowe11 <vladislavkim566@gmail.com>
2024-12-20 22:53:58 +0500

    feat: make registration view form and binding

--- matiz/views/registrationView.swift
+++ matiz/views/registrationView.swift
@@ -1,19 +1,24 @@
 import SwiftUI
 
 struct registrationView: View {
+    // todo: fix registration view
     @AppStorage("isRegistered") private var isRegistered: Bool = false
-    @State var login:String = ""
-    @State var password:String = ""
+    @State private var login:String = ""
+    @State private var password:String = ""
     var body: some View {
         VStack{
             TextField("login", text: $login)
                 .border(Color.black, width: 1)
+                .autocapitalization(.none)
             TextField("password", text: $password)
                 .border(Color.black, width: 1)
+                .autocapitalization(.none)
             HStack{
                 Button {
                     func register(){
-                        return
+                        if login == "test" && password == "test"{
+                            isRegistered = true
+                        }
                     }
                 } label: {
                     Text("submit")
