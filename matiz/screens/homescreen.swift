4197e66e022a8b4bfe6a6d51698e423ce9e5b701
llowe11 <vladislavkim566@gmail.com>
2024-12-22 13:51:01 +0500

    fix: fix homescreen content:

--- matiz/screens/homescreen.swift
+++ matiz/screens/homescreen.swift
@@ -4,7 +4,7 @@ struct homescreen: View {
     var body: some View {
         VStack{
             headerView()//todo:position
-            Text("there is homescreen")
+            Text("this is homescreen")
         }
     }
 }
