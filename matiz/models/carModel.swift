c762847fae685838a8bb7d7f9951d9058bc86a0f
llowe11 <vladislavkim566@gmail.com>
2024-12-24 20:07:19 +0500

    feat: create a carsArray and car model

--- matiz/models/carModel.swift
+++ matiz/models/carModel.swift
@@ -6,13 +6,15 @@ class carModel{
     var id = UUID()
     var brand:String
     var name:String
-    var date: Date
+    var date: String
     var mileAge: String
-    init(id: UUID = UUID(), brand: String, name: String, date: Date, mileAge: String) {
+    var carImg : String
+    init(id: UUID = UUID(), brand: String, name: String, date: String, mileAge: String, carImg:String) {
         self.id = id
         self.brand = brand
         self.name = name
         self.date = date
         self.mileAge = mileAge
+        self.carImg = carImg
     }
 }
