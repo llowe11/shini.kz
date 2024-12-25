049b84fb66a6ffd45e9a3e1795274d5ae4ddc6ee
llowe11 <vladislavkim566@gmail.com>
2024-12-25 20:58:17 +0500

    feat: create a lazy grid categories layout

--- matiz/views/homeviews/categoriesView.swift
+++ matiz/views/homeviews/categoriesView.swift
@@ -1,16 +1,27 @@
 import SwiftUI
 
 struct categoriesView: View {
+    let columns = [
+        GridItem(.flexible(), spacing: 10),
+        GridItem(.flexible(), spacing: 10),
+        GridItem(.flexible(), spacing: 10),
+        GridItem(.flexible(), spacing: 10)
+    ]
+    
     var body: some View {
-        //todo use lazy grid to implement categories tiles
         RoundedRectangle(cornerRadius: 20)
-            .stroke(Color.gray,lineWidth: 2)
-            .padding(.horizontal,20)
+            .fill(.white)
+            .overlay(
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(0..<8) { _ in
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 1)
+                            .aspectRatio(0.9, contentMode: .fit) // Slightly taller tiles
+                    }
+                }
+                .padding(5)
+            )
+            .padding(.horizontal, 20)
             .frame(height: 250)
-            .clipped()
     }
-}
-
-#Preview {
-    categoriesView()
 }
