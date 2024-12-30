e7d263266ab2bcda52bdccdaa7999b5a11e17058
llowe11 <vladislavkim566@gmail.com>
2024-12-30 17:51:25 +0500

    feat: create a car screen view

--- matiz/views/carview/carView.swift
+++ matiz/views/carview/carView.swift
@@ -5,6 +5,7 @@ import SwiftUI
 //todo: implement car screen
 struct carView: View {
     var body: some View {
+        Text("car image")
         Text("car name")
         Text("car brand")
         Text("car price")
