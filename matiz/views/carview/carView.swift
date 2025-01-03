091d8efae2f96c450da3b1a4923ae3a50d34b5db
llowe11 <vladislavkim566@gmail.com>
2025-01-03 11:36:17 +0500

    fix: app crash fix

--- matiz/views/carview/carView.swift
+++ matiz/views/carview/carView.swift
@@ -4,6 +4,7 @@ import SwiftData
 struct carView: View {
     var car: carModel
     var body: some View {
+        Text("car info")
         Text(car.name)
         Text(car.brand)
         Text(car.carImg)
