4cb6199ff4017f5166862115a777f9352fbfa96d
llowe11 <vladislavkim566@gmail.com>
2025-01-10 19:23:50 +0500

    feat: edit add cars screen

--- matiz/screens/addcarscreen.swift
+++ matiz/screens/addcarscreen.swift
@@ -2,6 +2,9 @@ import SwiftUI
 
 struct addcarscreen: View {
     var body: some View {
+//        VStack{
+//            addscarview()
+//        }
         VStack{
             Text("coming soon...")
         }
