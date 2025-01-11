803e2cdc0b1d97d777f17e11ab5692005adf1f14
llowe11 <vladislavkim566@gmail.com>
2025-01-11 16:38:52 +0500

    feat: edit carfeed tile structure preview

--- matiz/views/homeviews/feed/carTile.swift
+++ matiz/views/homeviews/feed/carTile.swift
@@ -3,13 +3,43 @@ import SwiftUI
 struct carTile: View {
     @State var car: carModel
     var body: some View {
+        let imageURL = URL(string: car.carImg)
             NavigationLink(destination: carView(car:car)){
                 Rectangle()
                     .stroke(Color.gray, lineWidth: 1)
                     .frame(height: 255)
                     .overlay(
-                        cartileText(car:car)
-                            .padding()
+                        VStack{
+                            cartileText(car:car)
+                                .padding(.horizontal,10)
+                            
+                            HStack{
+                                AsyncImage(url: imageURL) { phase in
+                                    switch phase {
+                                    case .empty:
+                                        ProgressView()
+                                            .frame(width: .infinity, height: 150)
+                                    case .success(let image):
+                                        image
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 200 , height: .infinity)
+                                            .cornerRadius(15)
+                                    case .failure:
+                                        Image(systemName: "photo")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: .infinity, height: 250)
+                                            .foregroundColor(.gray)
+                                    @unknown default:
+                                        EmptyView()
+                                    }
+                                }.padding(.horizontal)
+                                Spacer()
+                                Text("descriptoion")
+                                    .padding(.horizontal)
+                            }
+                        }
                     )
             }
     }
