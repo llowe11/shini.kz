b9e3aa86f3b9ec7ad31ec6a61c3c1e2a27a1eb41
llowe11 <vladislavkim566@gmail.com>
2025-01-13 21:49:13 +0500

    FEAT: create the monthly and initial payment calculation logic

--- matiz/views/carview/carView.swift
+++ matiz/views/carview/carView.swift
@@ -4,6 +4,9 @@ import SwiftData
 struct carView: View {
     var car: carModel
     var body: some View {
+        let monthlyPayment: Int? = Int(car.carprice).map { $0 / 12 }
+        let initialPayment: Double? = Int(car.carprice).map{ Double($0) * 0.1}
+        let formattedPayment = String(format: "%.0f", initialPayment ?? 0)
         let imageURL = URL(string: car.carImg)
         
         ScrollView {
@@ -50,12 +53,14 @@ struct carView: View {
                         HStack{
                             Text("Ежемесячный платеж")
                             Spacer()
-                            Text("100000")
+                            Text("\(monthlyPayment ?? 0) $")
+                                .background(Color.yellow)
+                                .padding(1)
                         }
                         HStack{
                             Text("Первоначальный взнос 10%")
                             Spacer()
-                            Text("100000")
+                            Text("\(formattedPayment) $")
                         }
                     }
                     Button {
@@ -70,7 +75,7 @@ struct carView: View {
                     }
                     .frame(height: 50)
                     .foregroundStyle(Color.red)
-                    .padding(.top,30)
+                    .padding(.top,10)
                 }
             }
         }
