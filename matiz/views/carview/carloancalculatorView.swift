c792cdc8a3bcf68dbac351fe5260925f73355c8d
llowe11 <vladislavkim566@gmail.com>
2025-01-15 21:13:11 +0500

    FEAT: add loan calculation button

---
+++ matiz/views/carview/carloancalculatorView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct carloancalculatorView: View {
+    var body: some View {
+        HStack{
+            Button {
+                return
+            } label: {
+                Text("10")
+            }
+
+        }
+    }
+}
+
+#Preview {
+    carloancalculatorView()
+}
