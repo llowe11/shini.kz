b89aa0403476bacd24432bde3278810fdbce242f
llowe11 <vladislavkim566@gmail.com>
2025-01-16 19:14:53 +0500

    FEAT: create and init car description model

--- matiz/models/cardescriptionModel.swift
+++ matiz/models/cardescriptionModel.swift
@@ -1,1 +1,13 @@
+import SwiftData
+import Foundation
 
+
+@Model
+class carDescription{
+    var id = UUID()
+    var isUsed:Bool
+    init(id: UUID = UUID(), isUsed: Bool) {
+        self.id = id
+        self.isUsed = isUsed
+    }
+}
