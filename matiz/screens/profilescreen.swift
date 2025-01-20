dd360690817efd9f14bd4705130a447ed5d80d06
llowe11 <vladislavkim566@gmail.com>
2025-01-20 20:12:42 +0500

    FEAT: create profile view

--- matiz/screens/profilescreen.swift
+++ matiz/screens/profilescreen.swift
@@ -2,16 +2,7 @@ import SwiftUI
 
 struct profilescreen: View {
     var body: some View {
-        ZStack{
-            ScrollView{
-                VStack{
-                    HStack{
-                        Text("profile image")
-                        Text("name surname")
-                    }
-                }
-            }
-        }
+        profileView()
     }
 }
 
---
+++ matiz/views/profileviews/profileView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct profileView: View {
+    var body: some View {
+        ZStack{
+            ScrollView{
+                VStack{
+                    HStack{
+                        Text("profile image")
+                        Text("name surname")
+                    }
+                }
+            }
+            profileheaderView()
+        }
+    }
+}
+
+#Preview {
+    profileView()
+}
