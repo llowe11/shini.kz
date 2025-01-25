d614cdb4875807f32d0377f4f4db3466cb129a23
llowe11 <vladislavkim566@gmail.com>
2025-01-25 15:04:33 +0500

    FEAT: edit profile view

--- matiz/views/profileviews/profileView.swift
+++ matiz/views/profileviews/profileView.swift
@@ -5,7 +5,13 @@ struct profileView: View {
         ZStack(alignment: .top){
             ScrollView{
                 VStack(spacing: 10){
-                    Text("coming soon...")
+                    HStack{
+                        Circle().frame(width: 150,height: 150)
+                    }
+                    HStack(spacing: 10){
+                        Text("Name").font(.title3)
+                        Text("Surname").font(.title3)
+                    }
                 }
             }.padding(.top, 80)
             profileheaderView()
