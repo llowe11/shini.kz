b90265138283535d85717758b2838ef57480285d
llowe11 <vladislavkim566@gmail.com>
2025-01-27 19:09:33 +0500

    FEAT: edit profile view

--- matiz/views/profileviews/profileView.swift
+++ matiz/views/profileviews/profileView.swift
@@ -10,7 +10,7 @@ struct profileView: View {
                     }
                     HStack(spacing: 10){
                         Text("name").font(.title3)
-                        Text("Surname").font(.title3)
+                        Text("surname").font(.title3)
                     }
                     Divider()
                     HStack{
